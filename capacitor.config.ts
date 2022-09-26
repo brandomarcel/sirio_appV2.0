dd95583519ba2b8d4c194c601f73b3e29f35ded2
brandomarcel <brandocevallos@gmail.com>
2022-09-26 15:27:01 -0500

    primer commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'sirio_appV2.0',
+  appName: 'sirio_appV2.0',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
